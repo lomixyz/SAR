1b9550c5c4e19071ed2a4f3d8ce6a3fb2eea9d9f
lomixyz <lomi.0368@gmail.com>
2025-04-13 16:37:28 +0300

    Saudi Arabia New Symbol for Odoo17

---
+++ aa_sar_symbol/__manifest__.py
@@ -0,0 +1,38 @@
+{
+    "name": "Saudi Riyal (SAR) Currency Symbol | SAR Symbol",
+    "version": "17.0.1.0.0",
+    "description": "Saudi Riyal (SAR) Currency New Symbol",
+    "summary": "SAR New Symbol",
+    "author": "Allam Bushra",
+    "website": "https://www.linkedin.com/in/lomixyz/",
+    "license": "OPL-1",
+    "category": "Tools",
+    "depends": ["base","web"],
+    "data": [
+        "data/res_currency_data.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "aa_sar_symbol/static/src/css/style.css",
+        ],
+        "web.assets_frontend": [
+            "aa_sar_symbol/static/src/css/style.css",
+        ],
+        "web.assets_common": [
+            "aa_sar_symbol/static/src/css/style.css",
+        ],
+        "web.report_assets_common": [
+            "aa_sar_symbol/static/src/css/style.css",
+        ],
+        "web.report_assets_pdf": [
+            "aa_sar_symbol/static/src/css/style.css",
+        ],
+        "web.assets_qweb": [
+            "aa_sar_symbol/static/src/css/style.css",
+        ],
+        "point_of_sale._assets_pos": [
+            "aa_sar_symbol/static/src/css/style.css",
+        ],
+    },
+    "application": True,
+}
